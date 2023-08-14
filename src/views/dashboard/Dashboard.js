e78e6efb6c4df8a261a3f356ac41b23563f5ced3
varanasi48 <varanasimurthy48@gmail.com>
2023-08-14 21:21:20 +0530

    Update Dashboard.js

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -53,6 +53,8 @@ import avatar6 from 'src/assets/images/avatars/6.jpg'
 
 import WidgetsBrand from '../widgets/WidgetsBrand'
 import WidgetsDropdown from '../widgets/WidgetsDropdown'
+import { DocsExample } from 'src/components'
+
 
 const Dashboard = () => {
   const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)

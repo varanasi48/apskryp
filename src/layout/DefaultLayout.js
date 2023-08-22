b923f1ea4572d6735308abfd7e9047f9a92350f5
mastermansachin <mastermansachin@outlook.com>
2023-08-22 22:15:04 +0530

    layout

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
 
 const DefaultLayout = () => {
+  useEffect(() => {
+    const userData = localStorage.getItem('userData')
+      ? JSON.parse(localStorage.getItem('userData'))
+      : null
+    if (!userData) {
+      window.location.href = '/'
+    }
+  }, [])
   return (
     <div>
       <AppSidebar />

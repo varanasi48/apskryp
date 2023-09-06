6df02ce36a540308761f401154cd8c4fda8386a4
varanasi48 <varanasimurthy48@gmail.com>
2023-09-06 16:43:36 +0530

    footter

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,15 +5,15 @@ const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          Developed by Apsire
         </a>
-        <span className="ms-1">&copy; 2023 creativeLabs.</span>
+        
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          LBF &amp; Dashboard Template
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          LBF 
         </a>
       </div>
     </CFooter>

c1e2b82adea6159e8ae1747550b993e90b57a487
varanasi48 <varanasimurthy48@gmail.com>
2023-09-16 00:46:21 +0530

    loginpage

--- src/views/pages/login/loginsamp.js
+++ src/views/pages/login/loginsamp.js
@@ -1,117 +1,110 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {
   MDBBtn,
   MDBContainer,
   MDBRow,
   MDBCol,
-  MDBInput
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon
 }
 from 'mdb-react-ui-kit';
-import './styles.css';
-const  Loginsamp=()=> {
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Login=()=> {
+
+  const API_URL = process.env.REACT_APP_API_URL
+  const navigate = useNavigate();
+  const [phoneno, setPhoneno] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault()
+
+
+
+    try {
+      const response = await axios.post(`${API_URL}/login`, {
+        phoneno: phoneno.trim(),
+        password: password.trim(),
+      })
+
+      // Save the received data in user's browser (localStorage) and state
+      localStorage.setItem('userData', JSON.stringify(response.data))
+      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`
+      setUser(response.data)
+	  
+    console.log(response.data.usertype)
+    if(response.data.usertype=='admin'){
+      navigate(`/dashboard`);
+      
+    }
+    if(response.data.usertype=='branch_manager'){
+      navigate(`/dashboardb`);
+
+    }
+    if(response.data.usertype=='investor'){
+      navigate(`/dashboardi`);
+
+    }
+    } catch (err) {
+      // Handle the error by updating the error state
+      setError(err.response.data)
+    }
+  }
+
   return (
-    <>
-    <title>Login V1</title>
-    <meta charSet="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    {/*===============================================================================================*/}
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
-    {/*===============================================================================================*/}
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="vendor/bootstrap/css/bootstrap.min.css"
-    />
-    {/*===============================================================================================*/}
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="fonts/font-awesome-4.7.0/css/font-awesome.min.css"
-    />
-    {/*===============================================================================================*/}
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css" />
-    {/*===============================================================================================*/}
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="vendor/css-hamburgers/hamburgers.min.css"
-    />
-    {/*===============================================================================================*/}
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="vendor/select2/select2.min.css"
-    />
-    {/*===============================================================================================*/}
-    <link rel="stylesheet" type="text/css" href="css/util.css" />
-    <link rel="stylesheet" type="text/css" href="css/main.css" />
-    {/*===============================================================================================*/}
-    <div className="limiter">
-      <div className="container-login100">
-        <div className="wrap-login100">
-          <div className="login100-pic js-tilt" data-tilt="">
-            <img src="../images/logo-lg.png" alt="IMG" />
-          </div>
-          <form className="login100-form " id="form">
-            <span className="login100-form-title">Member Login</span>
-            <div className="wrap-input100 ">
-              <input
-                className="input100"
-                type="text"
-                name="phoneno"
-                placeholder="Email"
-              />
-              <span className="focus-input100" />
-              <span className="symbol-input100">
-                <i className="fa fa-envelope" aria-hidden="true" />
-              </span>
-            </div>
-            <div className="wrap-input100 " data-validate="Password is required">
-              <input
-                className="input100"
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-              <span className="focus-input100" />
-              <span className="symbol-input100">
-                <i className="fa fa-lock" aria-hidden="true" />
-              </span>
-            </div>
-            <div className="container-login100-form-btn">
-              <button
-                id="submit"
-                name="submit"
-                value="submit"
-                className="login100-form-btn"
-              >
+    <MDBContainer fluid style={{backgroundImage:"url('/lbfbg.gif')"}}>
+
+      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCol col='12'>
+
+          <MDBCard className='bg-dark text-white my-5 mx-auto ' style={{borderRadius: '3rem', maxWidth: '400px',opacity:'80%'}}>
+            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+
+              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+              <p className="text-white-50 mb-5">Please enter your login and password!</p>
+
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Email address'  name="phoneno"
+                    onChange={(e) => {
+                      setPhoneno(e.target.value)
+                    }} type="text" size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password'  name="password"
+                    onChange={(e) => {
+                      setPassword(e.target.value)
+                    }}  type='password' size="lg"/>
+
+              <p className="small mb-3 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+              <MDBBtn outline className='mx-2 px-5' color='white' size='lg' type="submit" onClick={handleLoginSubmit}>
                 Login
-              </button>
-            </div>
-            <div className="text-center p-t-12">
-              <span className="txt1">Forgot</span>
-              <a className="txt2" href="#">
-                Username / Password?
-              </a>
-            </div>
-            <div className="text-center p-t-136">
-              <i className="fa fa-long-arrow-left m-l-5" aria-hidden="true" />
-              <a className="txt2" href="../index.html">
-                Back
-              </a>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-    {/*===============================================================================================*/}
-    {/*===============================================================================================*/}
-    {/*===============================================================================================*/}
-    {/*===============================================================================================*/}
-    {/*===============================================================================================*/}
-  </>
-  
-  )
+              </MDBBtn>
+
+              <div className='d-flex flex-row mt-3 mb-5'>
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='facebook-f' size="lg"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='twitter' size="lg"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='google' size="lg"/>
+                </MDBBtn>
+              </div>
+
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
+  );
 }
 
-export default Loginsamp;
+export default Login

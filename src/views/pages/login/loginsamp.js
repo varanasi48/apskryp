cdb06159117b411892b770a2f383334e7f64a6cb
varanasi48 <varanasimurthy48@gmail.com>
2023-09-16 01:20:34 +0530

    logo

--- src/views/pages/login/loginsamp.js
+++ src/views/pages/login/loginsamp.js
@@ -7,7 +7,8 @@ import {
   MDBCard,
   MDBCardBody,
   MDBInput,
-  MDBIcon
+  MDBIcon,
+  MDBCardImage
 }
 from 'mdb-react-ui-kit';
 import axios from 'axios';
@@ -61,9 +62,10 @@ const Login=()=> {
     <MDBContainer fluid style={{backgroundImage:"url('/lbfbg.gif')"}}>
 
       <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCardImage src='/logo-lg.png' style={{width:"40%"}}/>
         <MDBCol col='12'>
 
-          <MDBCard className='bg-dark text-white my-5 mx-auto ' style={{borderRadius: '3rem', maxWidth: '400px',opacity:'80%'}}>
+          <MDBCard className='bg-dark text-white my-5 mx-auto ' style={{borderRadius: '3rem', maxWidth: '400px'}}>
             <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
 
               <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
